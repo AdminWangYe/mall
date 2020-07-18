dc78151bcb308e081a925aa4db861c2ab126dd6b
AdminWangYe <1473332533@qq.com>
2020-07-18 15:48:11 +0800

    使用SpringTask实现定时任务

---
+++ src/main/java/com/uestc/mall/config/SpringTaskConfig.java
@@ -0,0 +1,14 @@
+package com.uestc.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author: yyWang
+ * @create: 2020/7/18
+ * @description: 定时配置任务
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
